b979fe08916d5bce687f9fb2b7a35e6e8703fecb
Michael Carchesio <35103168+Michaelcarchesio99@users.noreply.github.com>
2019-07-22 00:08:15 +0200

    Aggiunta primi esercizi 'semplici'

---
+++ LowLevel/OrdinamentoVettore.py
@@ -0,0 +1,9 @@
+#DICHIARAZIONE VETTORE .
+vector = [1, -23, 4, -1]
+
+#ORDINAMENTO .
+vector.sort()
+
+#STAMPA VETTORE ORDINATO .
+for i in range(0, len(vector)):
+    print(vector[i], end = " ")
---
+++ LowLevel/PariDispari.py
@@ -0,0 +1,8 @@
+#DICHIARAZIONE VARIABILE .
+var = int(input("Inserisci un numero: "))
+
+#ESECUZIONE COMPITO .
+if( var % 2 == 0 ):
+    print("NUMERO PARI")
+else:
+    print("NUMERO DISPARI")
